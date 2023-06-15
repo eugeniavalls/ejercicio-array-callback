c0b4e509b343fa15db85a4f601b5ba5b251d2d86
eugeniavalls <e.vallsalfaro@gmail.com>
2023-06-15 21:09:13 +0200

    cambios casa

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
+
+const numbers = [1, 2, 3, 4];
+
+numbers.forEach((number, index) => {
+console.log(number * index); 
+});
+
+// Crea una función que reciba un array de números y devuelva cada número dividido por su índice en el array más 2
+
+const sumNumbers = [ 4, 6,8];
+
+sumNumbers.forEach ((number, index) =>{
+    console.log(number / index + 2);
+});
+
+// Crea una función que reciba un array de palabras y devuelva un array con las mismas palabras en mayúsculas.
+
+const  words = [ 'hola', 'adios' ];
+
+const result = words.map(word => {
+	return word.toUpperCase();
+});
+
+console.log(result); // [ ‘A’, ‘B’, ‘C’, ‘D’ ]
+
+    
